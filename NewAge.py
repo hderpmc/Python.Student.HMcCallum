b90daf502901bfc92fe6ad8d6f4f08635b323e15
hderpmc <butteredribs.hdmc@gmail.com>
2025-09-11 13:54:50 -0500

    Add files via upload

---
+++ NewAge.py
@@ -0,0 +1,14 @@
+# This program calculates your age in the year 2050.
+# Input:  None.
+# Output: Your current age followed by your age in 2050.
+
+currentAge = 33
+
+currentYear = 2025
+
+newAge = currentAge + (2050 - currentYear)
+
+print(f"My Current Age is {currentAge}")
+print(f"I will be {newAge} in 2050.")
+
+
