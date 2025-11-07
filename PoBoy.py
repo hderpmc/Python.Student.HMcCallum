2b6639d9e5843e8d099a999e73315b6d804eeb20
hderpmc <butteredribs.hdmc@gmail.com>
2025-11-07 13:48:25 -0600

    Update PoBoy.py

--- PoBoy.py
+++ PoBoy.py
@@ -1,24 +1,58 @@
-# Boudreaux & Thibodeaux's Po-Boy Shop Program
+print("Boudreaux & Thibodeaux's Po-Boy Shop \n")
+print("------------------------------------ \n")
+print("1. Catfish Poboy: $14.95")
+print("2. Roast Beef Poboy: $13.95")
+print("3. Sausage Poboy: $12.95")
+print("4. Gumbo: $4.95")
+print("\n ------------------------------------- \n")
 
-menu = {
-    1: ("Catfish Poboy:", 14.95),
-    2: ("Roast Beef Poboy:", 13.95),
-    3: ("Sausage Poboy:", 12.95),
-    4: ("Gumbo:", 4.95) 
-}
-choice, (item, price) = menu
-salesTax = 0.0945
+# Constants
+CATFISH = 14.95
+ROASTBEEF = 13.95
+SAUSAGE = 12.95
+GUMBO = 4.95
 
-print("Boudreaux & Thibodeaux's Po-Boy Shop")
+choice = int(input("What would you like to order? Type the appropriate number of the menu item: ")
+# order_num = int(reply)
 
-choice = int(input("What would you like to order? Type the appropriate number of the menu item: "))
-
-for choice, (item, price) in menu.items():
-    print(f"{choice}. {item}: ${price:.2f}")
-
-if choice in menu:
-    item, price = menu[choice]
-    total = price + (price * salesTax)
-    print(f"Your total is ${total:.2f}")
+# Decision Structure
+if choice == 1:
+    total = CATFISH
+elif choice == 2:
+    total = ROASTBEEF
+elif choice == 3:
+    total = SAUSAGE
+elif choice == 4:
+    total = GUMBO
 else:
-    print("Invalid selection. Please restart the program and select a valid menu item.")
+    print("I'm sorry. That was not an appropriate choice.")
+    total = 0
+
+# Calculations
+salesTax = 0.0945
+subTotal = total + (total * salesTax)
+print(f'Your total is ${subTotal:.2f}')
+# print("Your total is ${tot:.2f)".format(tot=subTotal))
+            
+# menu = {
+   # 1: ("Catfish Poboy:", 14.95),
+   # 2: ("Roast Beef Poboy:", 13.95),
+   # 3: ("Sausage Poboy:", 12.95),
+   # 4: ("Gumbo:", 4.95) 
+# }
+# choice, (item, price) = menu
+# salesTax = 0.0945
+
+# print("Boudreaux & Thibodeaux's Po-Boy Shop")
+
+# choice = int(input("What would you like to order? Type the appropriate number of the menu item: "))
+
+# for choice, (item, price) in menu.items():
+   # print(f"{choice}. {item}: ${price:.2f}")
+
+#if choice in menu:
+   # item, price = menu[choice]
+   # total = price + (price * salesTax)
+   # print(f"Your total is ${total:.2f}")
+# else:
+   # print("Invalid selection. Please restart the program and select a valid menu item.")
